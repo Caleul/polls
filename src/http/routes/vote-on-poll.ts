f6e277b12ecf6bb9664d32bc40d7e44ad3cff5f4
root <root@Ubuntu.myguest.virtualbox.org>
2024-02-08 08:52:23 -0300

    add realtime voting analytics with redis

--- src/http/routes/vote-on-poll.ts
+++ src/http/routes/vote-on-poll.ts
@@ -2,6 +2,8 @@ import { z } from "zod"
 import { prisma } from "../../lib/prisma"
 import { FastifyInstance } from "fastify"
 import { randomUUID } from "crypto"
+import { redis } from "../../lib/redis"
+import { voting } from "../../utils/voting-pub-sub"
 
 export async function voteOnPoll(app: FastifyInstance) {
     app.post('/polls/:pollId/votes', async (request, reply) => {
@@ -32,8 +34,15 @@ export async function voteOnPoll(app: FastifyInstance) {
             if (userPreviousVoteOnPoll && userPreviousVoteOnPoll.pollOptionId !== pollOptionId) {
                 await prisma.vote.delete({
                     where: {
-                        id: userPreviousVoteOnPoll
+                        id: userPreviousVoteOnPoll.id
                     }
+                })
+
+                const votes = await redis.zincrby(pollId, -1, userPreviousVoteOnPoll.pollOptionId)
+                
+                voting.publish(pollId, {
+                    pollOptionId: userPreviousVoteOnPoll.pollOptionId,
+                    votes: Number(votes),
                 })
             } else if (userPreviousVoteOnPoll) {
                 return reply.status(400).send({ message: 'You already voted on this poll.'})
@@ -57,6 +66,13 @@ export async function voteOnPoll(app: FastifyInstance) {
                 pollId,
                 pollOptionId,
             }
+        })
+
+        const votes = await redis.zincrby(pollId, 1, pollOptionId)
+
+        voting.publish(pollId, {
+            pollOptionId,
+            votes: Number(votes),
         })
 
     
